7ba94afcbda859e61730f614ae3024d1388a07b0
Jeevan Sai <54813113+jeevanakajv@users.noreply.github.com>
2021-02-27 12:09:46 +0530

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    Text DisplayInfo;
+
+    Rigidbody player;
+    public AudioSource Info;
+    void Start()
+    {
+        Info = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider Col)
+    {
+        if (Col.gameObject.CompareTag("roudy"))
+        {
+           // Col.gameObject.SetActive(false);
+            Info.Play();
+        }
+    }
+}
